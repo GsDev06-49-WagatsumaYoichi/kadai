b8162148cb8b22bf84b2a1f1316fac73371f88f6
我妻陽一 <waga4155@gmail.com>
2017-02-09 15:41:30 +0900

    2/9色々

---
+++ 08/detail.php
@@ -0,0 +1,61 @@
+<?php
+$id = $_GET["id"];
+//1.  DB接続します
+try {
+  $pdo = new PDO('mysql:dbname=gs_db;charset=utf8;host=localhost','root','');
+} catch (PDOException $e) {
+  exit('データベースに接続できませんでした。'.$e->getMessage());
+}
+//２．データ登録SQL作成
+$stmt = $pdo->prepare("SELECT * FROM gs_an_table WHERE id= :id");//バインド変数埋め込み
+$stmt->bindValue(":id",$id,PDO::PARAM_INT); //STR or INT PDO::はセキュリティ
+$status = $stmt->execute();
+//３．データ表示
+$view="";
+if($status==false){
+  //execute（SQL実行時にエラーがある場合）
+  $error = $stmt->errorInfo();
+  exit("ErrorQuery:".$error[2]);
+}else{
+  $res = $stmt->fetch(); //1レコード取得
+ 
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>POSTデータ登録</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body>
+
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+    <div class="navbar-header"><a class="navbar-brand" href="select.php">データ一覧</a></div>
+  </nav>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+<form method="post" action="update.php">
+  <div class="jumbotron">
+   <fieldset>
+    <legend>フリーアンケート</legend>
+     <label>名前：<input type="text" name="name" value="<?= $res["name"]?>"></label><br>
+     <label>Email：<input type="text" name="email" value="<?= $res["email"]?>"></label><br>
+     <label><textArea name="naiyou" rows="4" cols="40" > <?= $res["naiyou"]?> </textArea></label><br>
+     <input type="hidden" name="id" value="<?=$id?>">
+     <input type="submit" value="送信">
+    </fieldset>
+  </div>
+</form>
+<!-- Main[End] -->
+
+
+</body>
+</html>
---
+++ 08/update.php
@@ -0,0 +1,42 @@
+<?php
+//1.POSTでParamを取得
+$id     = $_POST["id"];
+$name   = $_POST["name"];
+$email  = $_POST["email"];
+$naiyou = $_POST["naiyou"];
+
+//2.DB接続など
+//2. DB接続します(エラー処理追加)
+try {
+  $pdo = new PDO('mysql:dbname=gs_db;charset=utf8;host=localhost','root','');
+} catch (PDOException $e) {
+  exit('DbConnectError:'.$e->getMessage());
+}
+
+
+//３．データ登録SQL作成
+$stmt = $pdo->prepare("UPDATE gs_an_table SET name=:name,email=:email,naiyou=:naiyou WHERE id=:id");
+$stmt->bindValue(':name', $name);
+$stmt->bindValue(':email', $email);
+$stmt->bindValue(':naiyou', $naiyou);
+$stmt->bindValue(':id', $id);
+$status = $stmt->execute();
+
+//４．データ登録処理後
+if($status==false){
+  //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+  $error = $stmt->errorInfo();
+  exit("QueryError:".$error[2]);
+}else{
+  //５．index.phpへリダイレクト
+  header("Location: select.php");
+  exit;
+}
+
+//3.UPDATE gs_an_table SET ....; で更新(bindValue)
+//　基本的にinsert.phpの処理の流れです。
+
+
+
+
+?>
